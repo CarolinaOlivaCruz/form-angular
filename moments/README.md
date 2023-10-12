984cce46b00f4338ca746fa948726d8a2aa93781
Carol <carol.oliva.cruz@gmail.com>
2023-10-12 01:11:32 -0300

    Initial project

--- moments/README.md
+++ moments/README.md
@@ -1,4 +1,4 @@
-# MomentsSpa
+# Moments
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.6.
 
--- moments/src/app/app.component.spec.ts
+++ moments/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'moments-spa'`, () => {
+  it(`should have as title 'moments'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('moments-spa');
+    expect(app.title).toEqual('moments');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('moments-spa app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('moments app is running!');
   });
 });
